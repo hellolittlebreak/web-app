77f4f3b288a58030f98ae84fe435aae87b874093
Dragos Ivanov <ivanov.dragos@yahoo.com>
2021-11-22 22:01:55 +0200

    Add tailwind css

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div class="bg-blue-1000 w-screen h-screen">
+      <header className="App-header">
+        Hello world
+      </header>
+      <body>
+
+      </body>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
